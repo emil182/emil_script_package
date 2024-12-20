a82af8efcc9c2f9fa4f9e5608f11eb21eba6484f
emil182 <77433813+emil182@users.noreply.github.com>
2024-12-20 21:10:23 +0200

    Add files via upload

--- openmultiboot_All_pythons.sh
+++ openmultiboot_All_pythons.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-#
 
-wget -O /var/volatile/tmp/openmultiboot_All_python.ipk "https://github.com/emil182/emil_script_package/raw/refs/heads/main/enigma2-plugin-extensions-openmultiboot_v1.3_all-pyhon.ipk"
-wait
-opkg install --force-overwrite /tmp/*.ipk
-wait
-rm -r /var/volatile/tmp/enigma2-plugin-extensions-openmultiboot_v1.3_all-pyhon.ipk
-wait
-sleep 2;
+#
+wget -O /tmp/openmultiboot_All_python.ipk "https://github.com/emil182/emil_script_package/raw/refs/heads/main/enigma2-plugin-extensions-openmultiboot_v1.3_all-pyhon.ipk"
+
+#
+opkg install --force-overwrite /tmp/openmultiboot_All_python.ipk
+
+#
+rm -f /tmp/openmultiboot_All_python.ipk
+
+#
+sleep 2
+
 exit 0
+
